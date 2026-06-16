f8d3979bb936f436fa63f11ad5839029139c481a
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-06-16 09:25:37 +0000

    Decrease timeouts for eviction

--- pkg/ck8s/management_cluster.go
+++ pkg/ck8s/management_cluster.go
@@ -103,10 +103,10 @@ func (m *Management) GetWorkloadCluster(ctx context.Context, clusterKey client.O
 		AllowDeletion:         true,
 		IgnoreDaemonsets:      true,
 		DeleteEmptydirData:    true,
-		Timeout:               60 * time.Minute,
-		EvictionTimeout:       30 * time.Minute,
-		EvictionRetryInterval: 5 * time.Minute,
-		GracePeriodSeconds:    600,
+		Timeout:               10 * time.Minute,
+		EvictionTimeout:       5 * time.Minute,
+		EvictionRetryInterval: 1 * time.Minute,
+		GracePeriodSeconds:    60,
 	})
 
 	workload := &Workload{
