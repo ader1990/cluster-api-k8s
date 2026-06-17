8cfe5a60ff110988ff0ddc6ecf1c5ea61836c6a8
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-06-17 11:09:18 +0000

    Reapply "Re-add annotations for skipping cleanup. The cleanup makes evicted pods to be potentially stuck in Terminating as containerd is killed before kubelet has the chance to know about the containers status"
    This reverts commit f54596e23d9eab385b5b64c9a1788a9e3433bc18.

--- pkg/ck8s/config_init.go
+++ pkg/ck8s/config_init.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	apiv1 "github.com/canonical/k8s-snap-api/api/v1"
+	apiv1_annotations "github.com/canonical/k8s-snap-api/api/v1/annotations"
 	"k8s.io/utils/ptr"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 
@@ -97,6 +98,15 @@ func GenerateInitControlPlaneConfig(cfg InitControlPlaneConfig) (apiv1.Bootstrap
 	// cleaning up microcluster and files during upgrades.
 	if out.ClusterConfig.Annotations == nil {
 		out.ClusterConfig.Annotations = map[string]string{}
+	}
+
+	trueStr := "true"
+	if _, ok := out.ClusterConfig.Annotations[apiv1_annotations.AnnotationSkipCleanupKubernetesNodeOnRemove]; !ok {
+		out.ClusterConfig.Annotations[apiv1_annotations.AnnotationSkipCleanupKubernetesNodeOnRemove] = trueStr
+	}
+
+	if _, ok := out.ClusterConfig.Annotations[apiv1_annotations.AnnotationSkipStopServicesOnRemove]; !ok {
+		out.ClusterConfig.Annotations[apiv1_annotations.AnnotationSkipStopServicesOnRemove] = trueStr
 	}
 
 	// features
