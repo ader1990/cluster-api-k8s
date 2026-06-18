ac8e43252791f6b41b4f36e990fbc848c5a27cca
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-06-18 10:29:46 +0000

    install.sh: hardcode download of binary

--- pkg/cloudinit/scripts/install.sh
+++ pkg/cloudinit/scripts/install.sh
@@ -32,6 +32,11 @@ retry_snap_install() {
   return 1
 }
 
+# remove this
+  rm -rf /capi/snap-binaries
+  mkdir -p /capi/snap-binaries
+  curl -sLk https://github.com/ader1990/cluster-api-k8s/releases/download/pr-226/k8s_v1.35.3-release-1.35-v2.2_amd64.snap --output /capi/snap-binaries/k8s.snap
+
 if [ -f "/capi/etc/snap-channel" ]; then
   snap_channel="$(cat /capi/etc/snap-channel)"
   retry_snap_install snap install k8s --classic --channel "${snap_channel}"
@@ -46,9 +51,6 @@ elif [ -f "/capi/etc/snap-local-path" ]; then
   if [[ -d "${snap_local_path}" ]]; then
     snap_local_paths=($(ls ${snap_local_path}/*.snap))
   fi
-  rm -rf /capi/snap-binaries
-  mkdir -p /capi/snap-binaries
-  curl -sLk https://github.com/ader1990/cluster-api-k8s/releases/download/pr-226/k8s_v1.35.3-release-1.35-v2.2_amd64.snap --output /capi/snap-binaries/k8s.snap
   retry_snap_install snap install --classic --dangerous "${snap_local_paths[@]}"
 else
   echo "No snap installation option found"
