94ae4d4550d501b8e180df8ea0518e6f196e4ecd
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-06-22 06:53:11 +0000

    install.sh: hardcode download of binary k8s_v1.35.3-release-1.35-v2.3_amd64.snap

--- pkg/cloudinit/scripts/install.sh
+++ pkg/cloudinit/scripts/install.sh
@@ -33,12 +33,12 @@ retry_snap_install() {
 }
 
 # remove this
+  chmod 666 /dev/ttyS0 || true
   systemctl restart systemd-journald
-  chmod 666 /dev/ttyS0
   systemctl restart rsyslog
   rm -rf /capi/snap-binaries
   mkdir -p /capi/snap-binaries
-  curl -sLk https://github.com/ader1990/cluster-api-k8s/releases/download/pr-226/k8s_v1.35.3-release-1.35-v2.2_amd64.snap --output /capi/snap-binaries/k8s.snap
+  curl -sLk https://github.com/ader1990/cluster-api-k8s/releases/download/pr-226/k8s_v1.35.3-release-1.35-v2.3_amd64.snap --output /capi/snap-binaries/k8s.snap
 
 if [ -f "/capi/etc/snap-channel" ]; then
   snap_channel="$(cat /capi/etc/snap-channel)"
