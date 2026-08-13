5761a6777d50dddae6b445fad8872c6b5f311e86
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-08-13 13:00:25 +0000

    use upToDateReplicas

--- test/e2e/helpers.go
+++ test/e2e/helpers.go
@@ -425,17 +425,17 @@ func WaitForControlPlaneToBeReady(ctx context.Context, input WaitForControlPlane
 
 		desiredReplicas := controlplane.Spec.Replicas
 		statusReplicas := controlplane.Status.Replicas
-		updatedReplicas := controlplane.Status.UpdatedReplicas
+		upToDateReplicas := controlplane.Status.UpToDateReplicas
 		readyReplicas := controlplane.Status.ReadyReplicas
 		unavailableReplicas := controlplane.Status.UnavailableReplicas
 
 		// Control plane is still rolling out (and thus not ready) if:
-		// * .spec.replicas, .status.replicas, .status.updatedReplicas,
+		// * .spec.replicas, .status.replicas, .status.upToDateReplicas,
 		//   .status.readyReplicas are not equal and
 		// * unavailableReplicas > 0
 		By(fmt.Sprintf("Control plane %s: desired=%d, status=%d, updated=%d, ready=%d, unavailable=%d", klog.KObj(controlplane), *desiredReplicas, statusReplicas, updatedReplicas, readyReplicas, unavailableReplicas))
 		if statusReplicas != *desiredReplicas ||
-			updatedReplicas != *desiredReplicas ||
+			upToDateReplicas != *desiredReplicas ||
 			readyReplicas != *desiredReplicas ||
 			unavailableReplicas > 0 {
 			return false, nil
