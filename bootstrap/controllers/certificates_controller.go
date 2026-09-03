f2a25c79deb3745ddb756bbcab108a6f529cdf69
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-09-03 08:32:07 +0000

    [PoC] Upgrade capi version to 1.13.4
    Major logical changes:
    
      * Usage of capi apis v1beta2 using capi libraries v1.13.4
      * Added v1beta3 version for all ck8s* CRDs
      * Usage of the new v1beta3 version in the controllers
      * Updated v1beta2 version with the new imports
      * Added v1beta2 to v1beta3 conversions
    
    Major implementation details:
    
      * CK8sControlPlane uses the new conditions from the v1beta2 capi contract
      * CK8sControlPlane has a new capi contract status property - required by the new capi contract
    
    Signed-off-by: Adrian Vladu <adrian.vladu@canonical.com>
    Signed-off-by: Adrian Vladu <avladu@cloudbasesolutions.com>

--- bootstrap/controllers/certificates_controller.go
+++ bootstrap/controllers/certificates_controller.go
@@ -11,14 +11,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
-	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 	bsutil "sigs.k8s.io/cluster-api/bootstrap/util"
 	"sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/patch"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	bootstrapv1 "github.com/canonical/cluster-api-k8s/bootstrap/api/v1beta2"
+	bootstrapv1 "github.com/canonical/cluster-api-k8s/bootstrap/api/v1beta3"
 	"github.com/canonical/cluster-api-k8s/pkg/ck8s"
 	utiltime "github.com/canonical/cluster-api-k8s/pkg/time"
 	"github.com/canonical/cluster-api-k8s/pkg/token"
@@ -78,7 +78,7 @@ func (r *CertificatesReconciler) Reconcile(ctx context.Context, req ctrl.Request
 		return ctrl.Result{}, err
 	}
 
-	if m.Status.NodeRef == nil {
+	if m.Status.NodeRef.Name == "" {
 		// If the machine does not have a node ref, we requeue the request to retry.
 		return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
 	}
